d6d7967519c7c392aff1c7ab964be9976f877db6
TaodongQiu <709481804@qq.com>
2018-11-04 02:12:26 +0800

    change

--- exercise/scriptlib2.js
+++ exercise/scriptlib2.js
@@ -1,42 +1,25 @@
 $(document).ready(function(){
-
-    $("button#hide_h2").click(function() {
-        $("h2").hide(500);
-    });
-
-    $("button#show_h2").click(function() {
-        $("h2").show(300);
-        $("h2").css("color","blue");
-        $("h2").html("You clicked me hard.");
-    });
-
-    $("button#clear_screen").click(function() {
-        var $x = $("container");
-        $x.empty();
-    });
-
     $("button#get_data").click(function() {
-        var items = [];
+        var table1_items = [];
         var i = 0;
         var airtable_read_endpoint = "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB";
         var dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
                $.each(result.records, function(key,value) {
-                   items = [];
-                       items.push(value.fields.Name);
-                       items.push(value.fields.Year);
-                       items.push(value.fields.Douban_rating);
-                       items.push(value.fields.Director);
-                       items.push(value.fields.Actors);
-                       items.push(value.fields.IMDB_link);
-                       items.push(value.fields.Box_Office);
-                       dataSet.push(items);
-                       console.log(items);
+                   table1_items = [];
+                       table1_items.push(value.fields.Name);
+                       table1_items.push(value.fields.Year);
+                       table1_items.push(value.fields.Douban_rating);
+                       table1_items.push(value.fields.Director);
+                       table1_items.push(value.fields.Actors);
+                       table1_items.push(value.fields.IMDB_link);
+                       table1_items.push(value.fields.Box_Office);
+                       table1_dataSet.push(table1_items);
                 }); // end .each
-                console.log(dataSet);
 
-             $('#example').DataTable( {
-                 data: dataSet,
+
+             $('#table1').DataTable( {
+                 data: table1_dataSet,
                  retrieve: true,
                  columns: [
                      { title: "Name",
@@ -56,7 +39,6 @@ $(document).ready(function(){
                  ]
              } );
         }); // end .getJSON
-     }); // end button
 
 
 
@@ -92,7 +74,4 @@ $(document).ready(function(){
 
           }); // end .getJSON
        }); // end button
-
-        // $.getJSON('http://localhost/d756a/data_export.json/Computer+TV', function(obj) {
-
 }); // document ready

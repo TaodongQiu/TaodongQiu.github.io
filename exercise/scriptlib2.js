84282647969f8cc7da50448e8c1e4c36b665af5a
TaodongQiu <709481804@qq.com>
2018-11-04 01:57:10 +0800

    change

--- exercise/scriptlib2.js
+++ exercise/scriptlib2.js
@@ -63,7 +63,7 @@ $(document).ready(function(){
          var table2_items = [];
          var i = 0;
          var airtable_read_endpoint =
-         "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Stage?api_key=keyTcsTzckqyBTlk8&view=Grid%20view";
+         "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?maxRecords=3&view=Grid%20view";
          var table2_dataSet = [];
          $.getJSON(airtable_read_endpoint, function(result) {
                 $.each(result.records, function(key,value) {

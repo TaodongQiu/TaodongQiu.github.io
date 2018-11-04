deda670e49c832316580789360521ca2c4889ad3
TaodongQiu <709481804@qq.com>
2018-11-04 16:15:54 +0800

    change2

---
+++ assignment4/cyberpunk.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+     $("button#roll_up").click(function() {
+       var table1_items = [];
+       var i = 0;
+       var airtable_read_endpoint = "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB&maxRecords=3&view=Grid%20view";
+       var table1_dataSet = [];
+       $.getJSON(airtable_read_endpoint, function(result) {
+              $.each(result.records, function(key,value) {
+                  table1_items = [];
+                      table1_items.push(value.fields.Name);
+                      table1_items.push(value.fields.Year);
+                      table1_items.push(value.fields.Director);
+                      table1_items.push(value.fields.Actors);
+                      table1_items.push(value.fields.Douban_rating);
+                      table1_items.push(value.fields.IMDB_link);
+                      table1_items.push(value.fields.Box_office);
+                      table1_dataSet.push(table1_items);
+                      console.log(table1_items);
+               }); // end .each
+               console.log(table1_dataSet);
+
+            $('#table1').DataTable( {
+                data: table1_dataSet,
+                retrieve: true,
+                columns: [
+                    { title: "Name",
+                      defaultContent:""},
+                    { title: "Year",
+                        defaultContent:"" },
+                    { title: "Director",
+                      defaultContent:"" },
+                    { title: "Actors",
+                      defaultContent:""},
+                    { title: "Douban rating",
+                        defaultContent:""},
+                    { title: "IMDB link",
+                      defaultContent:""},
+                    { title: "Box office",
+                        defaultContent:""},
+                ]
+            } );
+       }); // end .getJSON
+
+         var table2_items = [];
+         var i = 0;
+         var airtable_read_endpoint =
+         "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB&maxRecords=3&view=Grid%20view";
+         var table2_dataSet = [];
+         $.getJSON(airtable_read_endpoint, function(result) {
+                $.each(result.records, function(key,value) {
+                    table2_items = [];
+                        table2_items.push(value.fields.Name);
+                        table2_items.push(value.fields.Box_office);
+                        table2_dataSet.push(table2_items);
+                        console.log(table2_items);
+                 }); // end .each
+                 console.log(table2_dataSet);
+                $('#table2').DataTable( {
+                    data: table2_dataSet,
+                    retrieve: true,
+                    ordering: false,
+                    columns: [
+                        { title: "Name",
+                          defaultContent:""},
+                        { title: "Box office",
+                          defaultContent:""},
+                    ] // rmf columns
+                } ); // end dataTable
+
+                var chart = c3.generate({
+                     data: {
+                         columns: table2_dataSet,
+                         type : 'bar'
+                     },
+                     bar: {
+                         title: "Tasks for Each Stage:",
+                     }
+                 });
+
+          }); // end .getJSON
+       }); // end button
+
+        // $.getJSON('http://localhost/d756a/data_export.json/Computer+TV', function(obj) {
+
+}); // document ready

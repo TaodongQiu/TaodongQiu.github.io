b83c30d18013c86408ad890a44541a76ea0d0504
TaodongQiu <709481804@qq.com>
2018-11-04 15:48:54 +0800

    222222

--- exercise/scriptlib2.js
+++ exercise/scriptlib2.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("button#get_data").click(function() {
         var table1_items = [];
         var i = 0;
-        var airtable_read_endpoint = "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB&maxRecords=3&view=Grid%20view";
         var dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
                $.each(result.records, function(key,value) {
@@ -45,7 +45,7 @@ $(document).ready(function(){
          var table2_items = [];
          var i = 0;
          var airtable_read_endpoint =
-         "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB";
+         "https://api.airtable.com/v0/app2R88MfJ0gFM8n4/Movies?api_key=key0WrAysAprBhXUB&maxRecords=3&view=Grid%20view";
          var table2_dataSet = [];
          $.getJSON(airtable_read_endpoint, function(result) {
                 $.each(result.records, function(key,value) {
